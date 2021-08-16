d12b79553f1ef75c9801c65d03bdb76e3dc7f763
Temi-Tegbe <temiloluwat@gmail.com>
2021-08-16 13:29:39 +0100

    define song entity

---
+++ TheBulbTunes/EFDataService/Entities/Song.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBulbTunes.EFDataService.Entities
+{
+  public  class Song
+    {
+        [Required]
+        public Guid SongId { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Artist { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Album { get; set; }
+
+        [MaxLength(150)]
+        public string Featuring { get; set; }
+
+        [MaxLength(50)]
+        public string Genre { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
