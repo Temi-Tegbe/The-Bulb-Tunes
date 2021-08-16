e8353d242ff0c9140f41904638a58247ae0b067a
Temi-Tegbe <temiloluwat@gmail.com>
2021-08-16 13:30:37 +0100

    define favourite entity

---
+++ TheBulbTunes/EFDataService/Entities/Favourite.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBulbTunes.EFDataService.Entities
+{
+  public  class Favourite
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid SelectedSongId { get; set; }
+
+
+        [Required]
+        public Guid AddedById { get; set; }
+
+        [Required]
+        public DataType DateAdded { get; set; }
+
+
+
+
+        //THE FOLLOWING ARE NAVIGATION PROPERTIES MADE POSSIBLE FOREIGN KEY RELATIONSHIPS
+
+        [Required]
+        public virtual Song SelectedSong { get; set; }
+
+        [Required]
+        public virtual User AddedBy { get; set; }
+    }
+}
